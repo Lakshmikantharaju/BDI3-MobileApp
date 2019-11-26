1c4c10a496de99a4064d820fe55f80aff3aa8c6f
Adeptpros <Adeptpros@DESKTOP-8PMDUJQ>
2019-11-26 16:01:38 +0530

    First commit

---
+++ BDI3/src/main/java/com/mobileapp/project/BDI3/App.java
@@ -0,0 +1,16 @@
+package com.mobileapp.project.BDI3;
+
+import org.testng.annotations.Test;
+
+/**
+ * Author- Lakshmikanth
+ * Test= Main Script
+ */
+public class App 
+{
+	@Test
+    public static void Method()
+    {
+    	System.out.println("Application launch has to be done in the main method");
+    }
+}
